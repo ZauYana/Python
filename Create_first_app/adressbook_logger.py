5cd2dbbf9bd65dd4b5118648b4fda6c2d37a63bc
ZauYana <zaulskayana@gmail.com>
2022-04-14 16:06:28 +0300

    not finished app

---
+++ Create_first_app/adressbook_logger.py
@@ -0,0 +1,3 @@
+def adress_book_logger (data):
+    with open ('log.csv','a') as file:
+        file.write ('{};')
---
+++ Create_first_app/input_contact.py
@@ -0,0 +1,24 @@
+# def create_new_contact (name,number,adress, date_of_birth):
+#     name = input("Введите имя контакта")
+#     number = input ("Введите номер контакта")
+#     adress = input ("Введите адрес контакта")
+#     date_of_birth = input ("Введите день рождение контакта")
+
+
+def save_name ():
+   name = input("Введите имя контакта ") 
+   return name
+
+def save_number ():
+   number = input ("Введите номер контакта") 
+   return number
+
+def save_adress ():
+   adress = input ("Введите адрес контакта") 
+   return adress
+
+def save_date_of_birth ():
+   date_of_birth = input ("Введите день рождение контакта")
+   return date_of_birth
+
+   
