81250293129dbbf52d3b3918625c600ee873a0dd
ZauYana <zaulskayana@gmail.com>
2022-04-14 22:19:41 +0300

    create_tlgbot

--- Lection5_Library/.folder/bot_commands.py
+++ Lection5_Library/.folder/bot_commands.py
@@ -18,23 +18,20 @@ def help_command(update: Update, context: CallbackContext):
 def time_command(update: Update, context: CallbackContext):
     log(update,context)
     update.message.reply_text(f'{datetime.datetime.now().time()}')
-
-def get_firstNum(update: Update, context: CallbackContext):
-    update.message.reply_text(f'Введите первое число:')
-    num1 = update.message.text
-    num1 = float(num1)
-
-def get_secondNum(update: Update, context: CallbackContext):
-    update.message.reply_text(f'Введите второе число:')
-    num2 = update.message.text
-    num2 = float(num2)
     
 def sum_command(update: Update, context: CallbackContext):
-    update.message.reply_text(f'Введите первое число:')
-    num1 = update.message.text
-    num1 = float(num1)
-    update.message.reply_text(f'Введите второе число:')
-    num2 = update.message.text
-    num2 = float(num2)
+    update.message.reply_text(f'Введите два числа через пробел:')
+    msg = update.message.text
+    items = msg.split()
+    num1 = int(items[1])
+    num2 = int(items[2])
     update.message.reply_text(f'{num1} + {num2} = {num1+num2}')
+
+def get_message (update: Update, context: CallbackContext):
+    msg = update.message.text
+    if msg != 'start' or 'help' or 'sum' or 'time':
+        update.message.reply_text(f'i know that i know nothing')
+    else:
+          update.message.reply_text(f'right choice') 
+
 
